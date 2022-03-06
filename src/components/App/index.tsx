474e839aad433a0adceefb069b583372c11ca544
ebedoyaalzate <esteban.bedoya@wolox.co>
2022-03-06 15:45:01 -0500

    add registerUser call to app

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import 'scss/application.scss';
 import SignUp from 'screens/SignUp';
 
 function App() {
-  return <SignUp />;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SignUp />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
