f6cd14726911bedc1facdc3abcd98b034dd9766f
ebedoyaalzate <esteban.bedoya@wolox.co>
2022-03-07 21:50:41 -0500

    add routes

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 import 'scss/application.scss';
 import SignUp from 'screens/SignUp';
+import Login from 'screens/Login';
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <SignUp />
+      <BrowserRouter>
+        <Route path="/" exact component={Login} />
+        <Route path="/sign-up" component={SignUp} />
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
