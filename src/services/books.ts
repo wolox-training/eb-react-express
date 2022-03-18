4a641e0ad24aee3c2d2024d3aa63cab3eb4e7ca3
ebedoyaalzate <esteban.bedoya@wolox.co>
2022-03-18 09:53:41 -0500

    fix types in services

--- src/services/books.ts
+++ src/services/books.ts
@@ -3,10 +3,4 @@ import { BookListResponse, Book } from '../types/book';
 
 export const getBooks = () => api.get<BookListResponse>('/books');
 
-export const getBookDetail = async (id: string): Promise<Book> => {
-  const response = await api.get<Book>(`/books/${id}`);
-  if (response.ok && response.data) {
-    return response.data;
-  }
-  throw new Error('Problem with data');
-};
+export const getBookDetail = (id: string) => api.get<Book>(`/books/${id}`);
